4aab694ba6b31991312868e3d9b362478b9461de
Nabiur Siddique <nabiursiddique01@gmail.com>
2026-09-16 23:25:28 +0900

    chore: set up the reactjs tailwind css and daisyui

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+
+  return (
+    <>
+      <div className="text-6xl">Hello world</div>
+      <button className="btn btn-xl">Xlarge</button>
+    </>
+  )
+}
+
+export default App
